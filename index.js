b2fa874f40c6174fae29d93c4cda4f8c04de6732
Dalo Barahama <das.barahama@gmail.com>
2021-10-22 23:29:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { response, request } = require('express')
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+app.get('/', (request, response) => {
+    return response.send('Hello, world!')
+})
+
+app.get('/todos', (request, response) => {
+    fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
+        if(err){
+            return response.status(500).send('Sorry, something went wrong.')
+        }
+
+        const todos = JSON.parse(data)
+        return response.json({todos: todos})
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Application running on http://localhost:3000')
+})
